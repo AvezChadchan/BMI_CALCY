d2cfa28a6d0f49775e8f9d59f147415e83bf219e
Avez Chadchan <avezchadchan@gmail.com>
2025-03-25 00:11:51 +0530

    .

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Dehimo',
+      title: 'Flutter Demo',
       theme: ThemeData(
         useMaterial3: true,
       ),
